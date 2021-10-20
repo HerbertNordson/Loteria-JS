f934f91f19932725b6947200c73ed1740056e48f
Herbert Nordson <herbert_nordson@hotmail.com>
2021-10-20 15:59:37 -0300

    feat: criação de loop para numeros e ações dos btn

--- index.js
+++ index.js
@@ -1,43 +1,73 @@
 (function (window, document, DOM) {
 	'use strict';
 
-	let $buttonType = document.querySelectorAll('[data-js="game-type"] > button');
-	let $type;
-	var $descption = new DOM('[data-js="game-info"]');
+	let $numbers = document.querySelector('[data-js="game-numbers"]');
+	let $buttonType = new DOM('[data-js="game-type"] > button').get();
+	let $buttonNumbers = new DOM('[data-js="game-numbers"] > button').get();
+	let numberArr = [];
+	
+	let $type = 'Lotofácil';
+	let $descption = document.querySelector('[data-js="game-info"]');
+	let $maxNumber;
+	let $price;
+	let $color;
+	let countMaxNumber = 1;
+
 
 	var ajax = new XMLHttpRequest();
 	ajax.open('GET', '/games.json');
 	ajax.send();
-	ajax.addEventListener('readystatechange', handleReadyStateChange);
-
-	function handleReadyStateChange() {
-		var result;
-
-		try {
-			result = JSON.parse(ajax.responseText);
-		}
-		catch (error) {
-			result = null;
-		}
-		
-		return result;
-	}
-
-	document.addEventListener('DOMContentLoaded', (ev) => {
-		ev.preventDefault();
-		ev = document.querySelector('[data-js="lotofacil"]');
-		$type = ev.value;
-		ev.click();
-	}, false);
+	ajax.addEventListener('readystatechange', parseData);
 
 	for (let typeGame of $buttonType) {
 		typeGame.addEventListener('click', (ev) => {
 			ev.preventDefault();
 			let element = ev.target.closest('button');
 			$type = typeGame.value;
+			cleanNumbers();
+			getData();
 			typeGameActive($type, element);
 		})
 	}
+
+	function getData() {
+		const type = parseData();
+		var description;
+		var numbers;
+		for (let i of type.types){
+			if (i.type === $type) {
+				description = i.description;
+				numbers = i.range;
+				$price = i.price;
+				$maxNumber = i["max-number"];
+				$color = i.color;
+			}
+		}
+
+		$descption.innerHTML = description;
+		createNumbersButton(numbers);
+	}
+
+	function cleanNumbers() {
+		return $numbers.innerHTML = '';
+	}
+
+	function createNumbersButton(number) {
+		for (let i = 1; i <= number; i++) {
+			const button = document.createElement("button");
+            $numbers.append(button);
+			button.innerHTML = `
+				${i}
+			`;
+		}
+	}
+
+	window.addEventListener('load', (ev) => {
+		ev.preventDefault();
+		ev = document.querySelector('[data-js="lotofacil"]');
+		$type = ev.value;
+		ev.click();
+	}, false);
 
 	function typeGameActive(game, target) {
 		let type = game;
@@ -60,30 +90,29 @@
 		if (ev.value === 'Mega-Sena') {
 			document.querySelector('[data-js="lotofacil"]').classList.remove('ativo');
     		document.querySelector('[data-js="lotomania"]').classList.remove('ativo');
-			getData(ev);
 		} else if (ev.value === 'Quina') {
 			document.querySelector('[data-js="mega-sena"]').classList.remove('ativo');
 			document.querySelector('[data-js="lotofacil"]').classList.remove('ativo');
-			getData(ev);
 		} else {
 			document.querySelector('[data-js="mega-sena"]').classList.remove('ativo');
 			document.querySelector('[data-js="lotomania"]').classList.remove('ativo');
-			getData(ev);
 		}
 
-		return ev.classList.toggle('ativo');
+		return ev.classList.add('ativo');
 
 	}
 
-	function getData(string) {
-		var data = handleReadyStateChange();
-		let types = //minha ideia vai ser utilizar um reduce aqui.;
-		console.log(types)
-		for (let i = 0; i < types.length; i++) {
-			if (types[i].type === string) {
-				$descption.get()[i].textContent = types[i].description;
-				console.log(types[i].description);
-			}
+	function parseData() {
+		var result;
+		
+		try {
+			result = JSON.parse(ajax.responseText);
 		}
+		catch (error) {
+			result = null;
+		}
+		
+		return result;
 	}
+
 })(window, document, window.DOM)
